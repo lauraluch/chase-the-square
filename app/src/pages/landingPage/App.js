e3b47690eb28b25b0b922ee43ce41fb0f5f8d127
lauraluch <lauraluchiari@hotmail.com>
2023-08-18 20:39:36 -0300

    initial commit

---
+++ app/src/pages/landingPage/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <div className="titleContainer">
+        <h1>Hey, would you like to chase a square?</h1>
+        <div className="textContainer">
+          <p>
+            This is a fun little experiment only for testing purposes.
+          </p>
+          <p>
+            Anyway, feel free to chase a square.
+          </p>
+        </div>
+      </div>
+      <div className="buttonContainer">
+        <button className="button">Chase it!</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
